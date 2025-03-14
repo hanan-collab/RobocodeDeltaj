95ac398f7c1e5055a594e80b6e9250f2ec741676
Hanan Adipratama <hananadipratama21@gmail.com>
2025-03-14 23:01:34 +0700

    New Feature + Variant with Near Targeting

--- RoboRobo/src/jab/module/Targeting.java
+++ RoboRobo/src/jab/module/Targeting.java
@@ -1,25 +1,30 @@
-package jab.module; 
+package jab.module;
+
+import robocode.util.Utils;
 
 /**
  * Targeting
  * 
  * @author jabier.martinez
  */
-public  class  Targeting  extends Part {
-	
+public class Targeting extends Part {
 
 	public Module bot;
-
-	
 
 	public Targeting(Module bot) {
 		this.bot = bot;
 	}
 
-	
-
 	public void target() {
-	}
+		if (bot.enemy != null) {
+			double absoluteBearing = bot.getHeadingRadians() + bot.enemy.bearingRadians;
+			double relativeBearing = Utils.normalRelativeAngle(absoluteBearing - bot.getGunHeadingRadians());
 
+			// Only target if the enemy is roughly to the left (-90 degrees) or right (90 degrees)
+			if (Math.abs(relativeBearing) > Math.PI / 4 && Math.abs(relativeBearing) < 3 * Math.PI / 4) {
+				bot.setTurnGunRightRadians(relativeBearing);
+			}
+		}
+	}
 
 }
