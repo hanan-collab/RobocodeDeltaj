f805e1cae7d2e9116d8bb3cf19c3b7e7a8c1dcb2
Athar Adista <athar0165@gmail.com>
2025-03-14 19:37:01 +0700

    Finish convert to delta

--- RoboRobo/src/jab/module/Movement.java
+++ RoboRobo/src/jab/module/Movement.java
@@ -1,5 +1,10 @@
 package jab.module;
 
+import robocode.AdvancedRobot;
+import robocode.Event;
+import robocode.HitWallEvent;
+import robocode.StatusEvent;
+
 /**
  * Movement
  * 
@@ -14,7 +19,34 @@ public class Movement extends Part {
 	}
 
 	public void move() {
-		bot.setAhead(0.0002);
+		bot.setMaxVelocity(8);
+		bot.setAhead(MOVE_DISTANCE * moveDirection);
+	}
+
+	private int moveDirection = 1;
+	private static final int MOVE_DISTANCE = 400;
+	private double traveledDistance = 0;
+
+	public void listen(Event e) {
+		if (e instanceof HitWallEvent) {
+			moveDirection *= -1;
+			bot.setBack(50); // Mundur sedikit
+			bot.setTurnRight(90); // Belok kanan
+			bot.execute();
+			traveledDistance = 0; // Reset jarak tempuh
+			bot.setAhead(MOVE_DISTANCE * moveDirection);
+		}
+
+		if (e instanceof StatusEvent) {
+			traveledDistance += Math.abs(bot.getVelocity());
+			System.out.println("Traveled Distance: " + traveledDistance);
+
+			if (traveledDistance >= MOVE_DISTANCE) {
+				moveDirection *= -1; // Ganti arah
+				traveledDistance = 0; // Reset jarak tempuh
+				bot.setAhead(MOVE_DISTANCE * moveDirection); // Pastikan perintahnya benar
+			}
+		}
 	}
 
 }
--- RoboRobo/src/jab/module/SelectEnemy.java
+++ RoboRobo/src/jab/module/SelectEnemy.java
@@ -1,25 +1,35 @@
-package jab.module; 
+package jab.module;
+
+import java.util.Iterator;
+import jab.module.BotInfo;
+import jab.module.Module;
+import jab.module.SelectEnemy;
 
 /**
  * Select enemy
  * 
  * @author jabier.martinez
  */
-public class  SelectEnemy  extends Part {
-	
+public class SelectEnemy extends Part {
 
 	public Module bot;
-
-	
 
 	public SelectEnemy(Module bot) {
 		this.bot = bot;
 	}
 
-	
-
 	public void select() {
+		Iterator<BotInfo> iterator = bot.botsInfo.values().iterator();
+		double minDistance = Double.MAX_VALUE;
+		BotInfo selected = null;
+		while (iterator.hasNext()) {
+			BotInfo botInfo = iterator.next();
+			if ((!botInfo.teammate) && minDistance > botInfo.distance) {
+				selected = botInfo;
+				minDistance = botInfo.distance;
+			}
+		}
+		bot.enemy = selected;
 	}
-
 
 }
